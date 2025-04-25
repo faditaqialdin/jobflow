28e1e4e013b2feacdb475c07d2049c039d352eed
Mohammad Fadi Taqi Al-Din <m.fadi313@gmail.com>
2025-04-26 01:25:23 +0300

    user MustVerifyEmail

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Database\Factories\UserFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -23,7 +24,7 @@ use Illuminate\Support\Str;
  * @property string $recommend_job_avoids
  * @property string $recommend_job_locations
  */
-class User extends Authenticatable // implements MustVerifyEmail
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
