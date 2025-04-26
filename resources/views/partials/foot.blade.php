865b8ccf886df1267b471a8dfaf5733e68fabef0
Mohammad Fadi Taqi Al-Din <m.fadi313@gmail.com>
2025-04-26 04:02:37 +0300

    add cookie consent

--- resources/views/partials/foot.blade.php
+++ resources/views/partials/foot.blade.php
@@ -1,3 +1,4 @@
+@cookieconsentview
 <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/sort@3.x.x/dist/cdn.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 @fluxScripts
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -8,6 +8,12 @@
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
+@cookieconsentscripts
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
 @stack('styles')
-
+<style>
+    .cookiesBtn__link {
+        background: #1447e6 !important;
+        border: 1px solid #1447e6 !important;
+    }
+</style>
